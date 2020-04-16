c792fe7135069cbb5c56a9d10484331591cb2e70
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-16 18:49:09 +0300

    Import story brain and create an instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+import 'story_brain.dart';
+import 'story_brain.dart';
+import 'story_brain.dart';
 
 void main() => runApp(Destini());
 
@@ -13,7 +14,7 @@ class Destini extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+StoryBrain brain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
