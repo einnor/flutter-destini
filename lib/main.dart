103dcb76af522145ae5ae587f23fc4cf3607c576
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-16 18:56:09 +0300

    Show choice 2

--- lib/main.dart
+++ lib/main.dart
@@ -84,8 +84,7 @@ class _StoryPageState extends State<StoryPage> {
                     },
                     color: Colors.blue,
                     child: Text(
-                      //TODO: Step 14 - Use the storyBrain to get the text for choice 1.
-                      'Choice 2',
+                      brain.getChoice2(),
                       style: TextStyle(
                         fontSize: 20.0,
                       ),
